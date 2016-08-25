cf911d7ff82450747f4e6e0c9319573da5e424b8
Hakchin Kim <sn2ro@eerfmai.local>
2016-08-26 00:04:09 +0900

    abc

--- ImageView/ViewController.swift
+++ ImageView/ViewController.swift
@@ -12,10 +12,10 @@ class ViewController: UIViewController {
     var isZoom = false
     var imgOn:   UIImage?
     var imgOff:  UIImage?
-
-    @IBOutlet weak var imgView: UIImageView!
+    
+    @IBOutlet weak var ivwLamp: UIImageView!
     @IBOutlet weak var btnResize: UIButton!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,28 +23,28 @@ class ViewController: UIViewController {
         imgOn  = UIImage(named: "lamp_on.png")
         imgOff = UIImage(named: "lamp_off.png")
         
-        imgView.image = imgOn
+        ivwLamp.image = imgOn
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func btnResizeImage(sender: UIButton) {
         let scale:CGFloat = 2.0
         var newWidth:CGFloat, newHeight:CGFloat
         
         if (isZoom){ //true
-            newWidth = imgView.frame.width/scale
-            newHeight = imgView.frame.height/scale
-            imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+            newWidth = ivwLamp.frame.width/scale
+            newHeight = ivwLamp.frame.height/scale
+            ivwLamp.frame.size = CGSize(width: newWidth, height: newHeight)
             btnResize.setTitle("확대", forState: .Normal)
         }
         else{ //false
-            newWidth = imgView.frame.width*scale
-            newHeight = imgView.frame.height*scale
-            imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+            newWidth = ivwLamp.frame.width*scale
+            newHeight = ivwLamp.frame.height*scale
+            ivwLamp.frame.size = CGSize(width: newWidth, height: newHeight)
             btnResize.setTitle("축소", forState: .Normal)
         }
         isZoom = !isZoom
@@ -52,9 +52,9 @@ class ViewController: UIViewController {
     
     @IBAction func swhImageOnOff(sender: UISwitch) {
         if sender.on{
-            imgView.image = imgOn
+            ivwLamp.image = imgOn
         } else {
-            imgView.image = imgOff
+            ivwLamp.image = imgOff
         }
     }
     
